02a1c7cae1bc69d92d7cfd2a13fd5436f7f3a6d3
dlovan tallat <dlovan.tallat@gmail.com>
2017-11-18 21:28:53 +0300

    Test the Project

--- app/src/main/java/com/dlovan/sunshine/MainActivity.kt
+++ app/src/main/java/com/dlovan/sunshine/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.dlovan.sunshine
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        message.text = "Welcome to Kotlin"
     }
 }
