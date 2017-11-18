e7609d90ecddfa1a9a0e88f4a061f3c52f5f2c14
dlovan tallat <dlovan.tallat@gmail.com>
2017-11-18 22:20:30 +0300

    feat: adding the adpater to the recyclerView to show forecast list

---
+++ app/src/main/java/com/dlovan/sunshine/adapters/ForecastListAdapter.kt
@@ -0,0 +1,26 @@
+package com.dlovan.sunshine.adapters
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import android.widget.TextView
+
+/**
+ * the Adapter for list of 7 days forecast
+ *
+ * Created by dlovan on 11/18/17.
+ */
+
+class ForecastListAdapter(private val items: List<String>) :
+        RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = items[position]
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
+            ViewHolder = ViewHolder(TextView(parent.context))
+
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+}
